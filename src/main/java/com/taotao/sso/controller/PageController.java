f42659165184814747632c5dbafd0a1b4df1564f
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-02-27 17:20:26 +1100

    adding redirect function on login

--- src/main/java/com/taotao/sso/controller/PageController.java
+++ src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,18 +1,20 @@
 package com.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
-    @RequestMapping("/login")
-    public String getLoginPage(){
+    @RequestMapping("/user/showLogin")
+    public String getLoginPage(String redirect, Model model) {
+        model.addAttribute("redirect", redirect);
         return "login";
     }
-    
+
     @RequestMapping("/user/showRegister")
-    public String showRegister(){
+    public String showRegister() {
         return "register";
     }
 }
